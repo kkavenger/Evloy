6ac904014b7077656609d5fcc3954b932637f11d
Kunal Mishra <avenger1.tonystark@gmail.com>
2024-01-09 02:09:49 +0530

    Working apis

--- api/index.js
+++ api/index.js
@@ -9,7 +9,41 @@ mongoose.connect(process.env.MONGO).then(() => {
     console.log(err);
 });
 
+const searchSchema = new mongoose.Schema({
+    keyword: String,
+    timestamp: { type: Date, default: Date.now },
+  });
+  
+const SearchEntry = mongoose.model('SearchEntry', searchSchema);
+
 const app = express();
+
+app.use(express.json());
+
+app.post('/api/search', async (req, res) => {
+    const { keyword } = req.body;
+  
+    try {
+      const newSearch = new SearchEntry({ keyword });
+      await newSearch.save();
+      res.status(201).json({ message: 'Search saved successfully.' });
+    } catch (error) {
+      console.error(error);
+      res.status(500).json({ error: 'Internal server error' });
+    }
+});
+  
+app.get('/api/searchVolume/:keyword', async (req, res) => {
+    const { keyword } = req.params;
+  
+    try {
+      const searchVolume = await SearchEntry.countDocuments({ keyword });
+      res.json({ keyword, searchVolume });
+    } catch (error) {
+      console.error(error);
+      res.status(500).json({ error: 'Internal server error' });
+    }
+});
 
 app.listen(5000, () => {
     console.log("Server listening on port 5000");
